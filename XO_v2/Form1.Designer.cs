2a27e649dac90dfb17e8ce035aa180357cb4f84a
CsizmaAle <133975054+CsizmaAle@users.noreply.github.com>
2024-08-29 12:25:24 +0300

    Add files via upload

---
+++ XO_v2/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace XO_atestat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button6 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button5 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonHighlight;
+            button1.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(235, 26);
+            button1.Name = "button1";
+            button1.Size = new Size(211, 192);
+            button1.TabIndex = 18;
+            button1.Tag = "";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(452, 26);
+            button2.Name = "button2";
+            button2.Size = new Size(211, 192);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(669, 26);
+            button3.Name = "button3";
+            button3.Size = new Size(211, 192);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(235, 224);
+            button4.Name = "button4";
+            button4.Size = new Size(211, 192);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(669, 224);
+            button6.Name = "button6";
+            button6.Size = new Size(211, 192);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.Location = new Point(669, 422);
+            button9.Name = "button9";
+            button9.Size = new Size(211, 192);
+            button9.TabIndex = 8;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.Location = new Point(1127, 94);
+            button10.Name = "button10";
+            button10.Size = new Size(62, 57);
+            button10.TabIndex = 9;
+            button10.Text = "X";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button11.Location = new Point(1238, 94);
+            button11.Name = "button11";
+            button11.Size = new Size(62, 57);
+            button11.TabIndex = 10;
+            button11.Text = "0";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button12.Location = new Point(452, 224);
+            button12.Name = "button12";
+            button12.Size = new Size(211, 192);
+            button12.TabIndex = 14;
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button13.Location = new Point(235, 422);
+            button13.Name = "button13";
+            button13.Size = new Size(211, 192);
+            button13.TabIndex = 15;
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button14.Location = new Point(452, 422);
+            button14.Name = "button14";
+            button14.Size = new Size(211, 192);
+            button14.TabIndex = 16;
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.IndianRed;
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(1127, 540);
+            button5.Name = "button5";
+            button5.Size = new Size(192, 74);
+            button5.TabIndex = 19;
+            button5.Text = "MENIU";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(984, 187);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 31);
+            label1.TabIndex = 20;
+            label1.Text = "SCOR:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(1141, 187);
+            label2.Name = "label2";
+            label2.Size = new Size(26, 31);
+            label2.TabIndex = 21;
+            label2.Text = "0";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(1257, 187);
+            label3.Name = "label3";
+            label3.Size = new Size(26, 31);
+            label3.TabIndex = 22;
+            label3.Text = "0";
+            label3.Click += label3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
+            ClientSize = new Size(1377, 692);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button5);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button6;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ XO_v2/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Windows.Forms;
+
+namespace XO_atestat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        int[] b = new int[10];  // vector
+        bool x = false;
+        bool o = false;
+        int ok = 0;
+        int wino = 0;
+        int winx = 0;
+        int j = 1;
+
+        public void Verificare()
+        {
+            ok = 0;
+            int cnt = 0;
+            if (b[1] == b[2] && b[2] == b[3] && b[2] == 88) ok = 2;         // 1 2 3 x
+            else if (b[1] == b[2] && b[2] == b[3] && b[2] == 79) ok = 3;    // 1 2 3 o
+            else if (b[4] == b[5] && b[5] == b[6] && b[5] == 88) ok = 2;    // 4 5 6 x
+            else if (b[4] == b[5] && b[5] == b[6] && b[5] == 79) ok = 3;    // 4 5 6 o
+            else if (b[7] == b[8] && b[8] == b[9] && b[8] == 88) ok = 2;    // 7 8 9 x
+            else if (b[7] == b[8] && b[8] == b[9] && b[9] == 79) ok = 3;    // 7 8 9 o
+            else if (b[4] == b[1] && b[1] == b[7] && b[7] == 88) ok = 2;    // 1 4 7 x
+            else if (b[4] == b[1] && b[1] == b[7] && b[7] == 79) ok = 3;    // 1 4 7 o
+            else if (b[2] == b[5] && b[5] == b[8] && b[8] == 88) ok = 2;    // 2 5 8 x
+            else if (b[2] == b[5] && b[5] == b[8] && b[5] == 79) ok = 3;    // 2 5 8 o
+            else if (b[3] == b[6] && b[9] == b[6] && b[9] == 88) ok = 2;    // 3 6 9 x
+            else if (b[3] == b[6] && b[9] == b[6] && b[9] == 79) ok = 3;    // 3 6 9 o
+            else if (b[1] == b[5] && b[9] == b[5] && b[9] == 88) ok = 2;    // 1 5 9 x
+            else if (b[1] == b[5] && b[9] == b[5] && b[9] == 79) ok = 3;    // 1 5 9 o
+            else if (b[3] == b[5] && b[5] == b[7] && b[7] == 88) ok = 2;    // 3 5 7 x
+            else if (b[3] == b[5] && b[7] == b[5] && b[3] == 79) ok = 3;    // 3 5 7 o
+
+            // check if the vector is full
+            for (int i = 1; i <= 9; i++)
+                if (b[i] != 0) cnt++;
+
+            // end of the game
+            if (cnt == 9 && ok == 0)
+            {
+                ok = 1;
+                // we create a mesaje box to inform the players 
+                DialogResult result = MessageBox.Show("NOBODY WON! PLAY AGAIN?" + Environment.NewLine +
+                    "Score" + Environment.NewLine + "X: " + winx + Environment.NewLine + "0: " + wino,
+                    "game over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes) { restart(); }
+                else
+                {
+                    this.Close();
+                    Meniu meniu = new Meniu();
+                    meniu.Show();
+                    this.Close();
+                }
+            }
+            else
+                if (ok == 2)
+            {
+                winx++;
+                DialogResult result = MessageBox.Show("PLAYER X WINS! PLAY AGAIN?" + Environment.NewLine +
+                    "Score" + Environment.NewLine + "X: " + winx + Environment.NewLine + "0: " + wino, "congrats",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes) { restart(); }
+                else
+                {
+                    this.Close();
+                    Meniu meniu = new Meniu();
+                    meniu.Show();
+                    this.Close();
+                }
+            }
+            else
+                    if (ok == 3)
+            {
+                wino++;
+                DialogResult result = MessageBox.Show("PLAYER 0 WINS! PLAY AGAIN?" + Environment.NewLine + "Score" +
+                    Environment.NewLine + "X: " + winx + Environment.NewLine + "0: " + wino, "congrats",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes) { restart(); }
+                else
+                {
+                    this.Close();
+                    Meniu meniu = new Meniu();
+                    meniu.Show();
+                    this.Close();
+                }
+            }
+        }
+
+        public void restart()
+        {
+            for (int i = 1; i <= 9; i++) b[i] = 0;
+            disable();
+            button1.BackColor = Color.White;
+            button2.BackColor = Color.White;
+            button3.BackColor = Color.White;
+            button4.BackColor = Color.White;
+            button12.BackColor = Color.White;
+            button13.BackColor = Color.White;
+            button14.BackColor = Color.White;
+            button6.BackColor = Color.White;
+            button9.BackColor = Color.White;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            button1.Text = " ";
+            button2.Text = " ";
+            button3.Text = " ";
+            button4.Text = " ";
+            button12.Text = " ";
+            button13.Text = " ";
+            button14.Text = " ";
+            button6.Text = " ";
+            button9.Text = " ";
+            j = 1;
+            label2.Text = winx.ToString();
+            label3.Text = wino.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // box 1
+        {
+            if (x == true)
+            {
+                button1.Text = "X"; // place X
+                b[1] = 88; // note that in box 1 is X
+                button1.BackColor = Color.SkyBlue; // change button color
+            }
+
+            if (o == true)
+            {
+                button1.Text = "O"; // place O
+                b[1] = 79; // note O 
+                button1.BackColor = Color.LightPink; // change button color
+            }
+
+            disable();// disable the buttons
+            o = false; x = false; // reset selection
+            button10.BackColor = Color.White; // reset colors for button 11 and 10
+            button11.BackColor = Color.White;
+            Verificare(); // check if someone won 
+            joc(j++);
+        }
+
+        private void button2_Click(object sender, EventArgs e)// box 2
+        {
+            if (x == true) { button2.Text = "X"; b[2] = 88; button2.BackColor = Color.SkyBlue; }
+            if (o == true) { button2.Text = "O"; b[2] = 79; button2.BackColor = Color.LightPink; }
+            disable();
+            o = false; x = false;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            Verificare();
+            joc(j++);
+        }
+
+        private void button3_Click(object sender, EventArgs e) // box 3
+        {
+            if (x == true) { button3.Text = "X"; b[3] = 88; button3.BackColor = Color.SkyBlue; }
+            if (o == true) { button3.Text = "O"; b[3] = 79; button3.BackColor = Color.LightPink; }
+            disable();
+            o = false; x = false;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            Verificare();
+            joc(j++);
+        }
+
+        private void button4_Click(object sender, EventArgs e) // box 4
+        {
+            if (x == true) { button4.Text = "X"; b[4] = 88; button4.BackColor = Color.SkyBlue; }
+            if (o == true) { button4.Text = "O"; b[4] = 79; button4.BackColor = Color.LightPink; }
+            disable();
+            o = false; x = false;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            Verificare();
+            joc(j++);
+        }
+
+        private void button6_Click(object sender, EventArgs e) // box 6
+        {
+            if (x == true) { button6.Text = "X"; b[6] = 88; button6.BackColor = Color.SkyBlue; }
+            if (o == true) { button6.Text = "O"; b[6] = 79; button6.BackColor = Color.LightPink; }
+            disable();
+            o = false; x = false;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            Verificare();
+            joc(j++);
+        }
+
+        private void button9_Click(object sender, EventArgs e) // box 9
+        {
+            if (x == true) { button9.Text = "X"; b[9] = 88; button9.BackColor = Color.SkyBlue; }
+            if (o == true) { button9.Text = "O"; b[9] = 79; button9.BackColor = Color.LightPink; }
+            disable();
+            o = false; x = false;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            Verificare();
+            joc(j++);
+        }
+
+        private void button12_Click(object sender, EventArgs e) // box 5
+        {
+            if (x == true) { button12.Text = "X"; b[5] = 88; button12.BackColor = Color.SkyBlue; }
+            if (o == true) { button12.Text = "O"; b[5] = 79; button12.BackColor = Color.LightPink; }
+            disable();
+            o = false; x = false;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            Verificare();
+            joc(j++);
+        }
+
+        private void button14_Click(object sender, EventArgs e) // box 8
+        {
+            if (x == true) { button14.Text = "X"; b[8] = 88; button14.BackColor = Color.SkyBlue; }
+            if (o == true) { button14.Text = "O"; b[8] = 79; button14.BackColor = Color.LightPink; }
+            disable();
+            o = false; x = false;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            Verificare();
+            joc(j++);
+        }
+
+        private void button13_Click(object sender, EventArgs e) // box 7
+        {
+            if (x == true) { button13.Text = "X"; b[7] = 88; button13.BackColor = Color.SkyBlue; }
+            if (o == true) { button13.Text = "O"; b[7] = 79; button13.BackColor = Color.LightPink; }
+            disable();
+            o = false; x = false;
+            button10.BackColor = Color.White;
+            button11.BackColor = Color.White;
+            Verificare();
+            joc(j++);
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++) b[i] = 0;
+            label2.Text = winx.ToString();
+            label3.Text = wino.ToString();
+            disable(); // disable all buttons
+            joc(j++);
+        }
+
+        public void joc(int j)
+        {
+            if (j % 2 == 0)
+            {
+                o = true;
+                x = false;
+                button11.BackColor = Color.Blue;
+                button10.BackColor = Color.White;
+                enable();
+            }
+            else
+            {
+                o = false;
+                x = true;
+                button10.BackColor = Color.Blue;
+                button11.BackColor = Color.White;
+                enable();
+            }
+        }
+
+        public void disable() // disables all the buttons
+        {
+            button1.Enabled = false;
+            button12.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button6.Enabled = false;
+            button13.Enabled = false;
+            button14.Enabled = false;
+            button9.Enabled = false;
+        }
+
+        public void enable()
+        {
+            if (b[1] == 0) button1.Enabled = true;
+            if (b[2] == 0) button2.Enabled = true;
+            if (b[3] == 0) button3.Enabled = true;
+            if (b[4] == 0) button4.Enabled = true;
+            if (b[5] == 0) button12.Enabled = true;
+            if (b[6] == 0) button6.Enabled = true;
+            if (b[7] == 0) button13.Enabled = true;
+            if (b[8] == 0) button14.Enabled = true;
+            if (b[9] == 0) button9.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Meniu meniu = new Meniu();
+            meniu.Show();
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
